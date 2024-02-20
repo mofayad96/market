3e6c6abab34d76df9e0f653516620c493c76f8e0
NondescriptVector <160421099+NondescriptVector@users.noreply.github.com>
2024-02-20 08:18:02 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
 import java.util.*;
 
 public class Main {
-    //arraylists to track the order
-    static String[] itemname = {"apple", "orange", "berries"};
-    static double[] prices = {12, 10, 16};
-    static int[] quantity = {10, 12, 50};
+    //array Bought_ItemQuantity to track the order
     static int[] Bought_ItemQuantity = new int[3];
     static Scanner input = new Scanner(System.in); //made scanner static
-
+    
     public static void main(String[] args) {
         //muhammed essam
         String[][] users = {
@@ -42,7 +39,9 @@ public class Main {
         //muhammed essam end
 
         //sabrina & Lamice & youssef
-
+        String[] itemname = {"apple", "orange", "berries"};
+        double[] prices = {12, 10, 16};
+        int[] quantity = {10, 12, 50};
         double totalamount = 0;
         System.out.println("Available items:");
         for (int i = 0; i < prices.length; i++) {
@@ -53,7 +52,14 @@ public class Main {
             System.out.println("enter 1 to purchase item 1 , 2 to purchase item 2 , 3 to purchase item 3 & 0 to quit");
             int productnumber = input.nextInt();
             if (productnumber == 0) {
-                Receipt();
+                System.out.println("Product\t\tQuantity\t\tPrice\t\tValue");
+                for (int i = 0; i < Bought_ItemQuantity.length; i++) {
+                    //the ultimate receipt ,fathalla receipt
+                    if(Bought_ItemQuantity[i] == 0)
+                        continue;
+                    System.out.println(itemname[i]);
+                    System.out.println("\t\t\t" + Bought_ItemQuantity[i] + "\t\t\t\t" + prices[i] + "\t\t" + prices[i] * Bought_ItemQuantity[i]);
+                }
                 System.out.println("Your total is: " + totalamount);
                 System.out.println("thank you , come back later");
                 break;
@@ -73,23 +79,10 @@ public class Main {
             }
             //updating stock
             quantity[productnumber - 1] = quantity[productnumber - 1] - orderedquantity;
-            Bought_ItemQuantity[productnumber-1] = orderedquantity;//updating the shopping cart and total amount
+            Bought_ItemQuantity[productnumber-1] += orderedquantity;//updating the shopping cart and total amount
             totalamount += (prices[productnumber - 1] * orderedquantity);
         }
         //scanner close for good practice
         input.close();
-    }
-
-    static void Receipt() {
-
-        System.out.println("Product\t\tQuantity\t\tPrice\t\tValue");
-        for (int i = 0; i < Bought_ItemQuantity.length; i++) {
-            //the ultimate receipt ,fathalla receipt
-            if(Bought_ItemQuantity[i] == 0)
-                continue;
-            System.out.println(itemname[i]);
-            System.out.println("\t\t\t" + Bought_ItemQuantity[i] + "\t\t\t\t" + prices[i] + "\t\t" + prices[i] * Bought_ItemQuantity[i]);
-        }
-
     }
 }
