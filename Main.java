0b18ab37aee9780de882f49a7da5e7ddaba08095
mofayad96 <117763149+mofayad96@users.noreply.github.com>
2024-02-20 20:54:24 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,108 +1,106 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
+    static Scanner input = new Scanner(System.in); //made scanner static
+
     public static void main(String[] args) {
         //muhammed essam
-        Scanner input=new Scanner(System.in);
         String[][] users = {
                 {"Mohamed22", "10111"},
                 {"hossam22", "11011"},
                 {"saeed22", "11101"},
         };
+        //infinite while loop to make room for error
         while (true) {
             System.out.print("Please enter username: ");
             String Username = input.nextLine();
             System.out.print("Please enter the password: ");
             String Password = input.nextLine();
             boolean auth = false;
+
             // Check if entered username and password match
             for (String[] user : users) {
-                if (user[0].equals(Username) && user[1].equals(Password)) {
+                if (user[0].equalsIgnoreCase(Username) && user[1].equals(Password)) {
                     auth = true;
                     break; // Exit the loop if a match is found
                 }
             }
+
             // Check the auth.....
             if (auth) {
                 System.out.println("Authentication successful!");
                 break;
             } else {
                 System.out.println("Authentication failed. Invalid username or password.");
-            } }
+            }
+        }
         //muhammed essam end
 
-
-
-        //sabrina,jana
-        String [] itemname = {"apple","orange","berries"};
-        double [] prices= {12,10,16};
-        int [] quantity= {10,12,50};
-        double totalamount =0;
-
-        System.out.println("items  prices  quantity");
+        //jana
+        String[] itemname = {"apple", "orange", "berries"};
+        double[] prices = {12, 10, 16};
+        int[] quantity = {10, 12, 50};
+        double totalamount = 0;
+        System.out.println("Available items:");
         for (int i = 0; i < prices.length; i++) {
-            System.out.println(itemname[i] + "  " + prices[i] + "   " + quantity[i]);
+            System.out.println(itemname[i] + ", prices: " + prices[i] + ", available quantities: " + quantity[i]);
         }
-
-        // youssef , lamis
-        String[] purchasedItemNames = new String[itemname.length];
-        int[] purchasedQuantities = new int[itemname.length];
-        double[] purchasedTotals = new double[itemname.length];
-        int purchasedItemCount = 0;
-        //end youssef , lamis
-
-
-        // sabrina , jana
-        while(true){
+         int[] Bought_ItemQuantity = new int[itemname.length];
+        //also to make room for error
+        while (true) {
             System.out.println("enter 1 to purchase item 1 , 2 to purchase item 2 , 3 to purchase item 3 & 0 to quit");
-            int productnumber= input.nextInt();
+            int productnumber = input.nextInt();
+            // jana end
 
-            if(productnumber==0){
 
-                break ;
-            }
-            if ( productnumber<1 || productnumber>itemname.length ){
+            // YOUSSEF, lamis
+            if (productnumber == 0) {
+                System.out.println("Product\t\tQuantity\t\tPrice\t\tValue");
+                for (int i = 0; i < Bought_ItemQuantity.length; i++) {
+                    //the ultimate receipt ,fathallah receipt
+                    if(Bought_ItemQuantity[i] == 0)
+                        continue;
+                    System.out.println(itemname[i]);
+                    System.out.println("\t\t\t" + Bought_ItemQuantity[i] + "\t\t\t\t" + prices[i] + "\t\t" + prices[i] * Bought_ItemQuantity[i]);
+                }
+                System.out.println("Your total is: " + totalamount);
+                System.out.println("thank you , come back later");
+                break;
+            }//END YOUSSEF,lamis
+
+
+            // sabrina
+            //if the user entered an invalid number
+            if (productnumber < 1 || productnumber > itemname.length) {
                 System.out.println("invalid base try again");
-                continue ;
+                continue;
             }
-            int productquantity =quantity[productnumber-1];
+            //removed the product quantity because it was redundant
             System.out.println("please enter your requested quantity:");
-            int ordredquantity = input.nextInt();
-
-            if( ordredquantity < 1 || ordredquantity > productquantity ){
+            int orderedquantity = input.nextInt(); //it's ordered and not ordred ya sabrina
+            //also for invalid numbers
+            if (orderedquantity < 1 || orderedquantity > quantity[productnumber - 1]) {
                 System.out.println(" invalid quantity , try again");
-                continue ;
+                continue;
             }
-            //end of sabina , jana
-
-            // lamis , youssef
-            quantity[productnumber-1] =quantity[productnumber-1]-ordredquantity;
-            double itemTotal = prices[productnumber - 1] * ordredquantity;
-            totalamount += itemTotal;
-
-
-            purchasedItemNames[purchasedItemCount] = itemname[productnumber - 1];
-            purchasedQuantities[purchasedItemCount] = ordredquantity;
-            purchasedTotals[purchasedItemCount] = itemTotal;
-            purchasedItemCount++;
 
 
 
-        }
-        System.out.println("Purchased Items:");
-        for (int i = 0; i < purchasedItemCount; i++) {
-            System.out.println(purchasedItemNames[i] + " - Quantity: " + purchasedQuantities[i] + " - Total: $" + purchasedTotals[i]);
+            //sabrina
+            quantity[productnumber - 1] = quantity[productnumber - 1] - orderedquantity;
+            Bought_ItemQuantity[productnumber-1] += orderedquantity;//updating the shopping cart and total amount
+            totalamount =totalamount + (prices[productnumber - 1] * orderedquantity);
         }
 
-        System.out.println("Total Amount: $" + totalamount);
-        System.out.println("thank you , come back later");
-        System.out.println("                            ");
+
+        System.out.println("                    ");
         System.out.println("FOR ADMIN!!!!!!!!!");
         System.out.println("Updated items quantity:");
         System.out.println("items  prices  quantity");
         for (int i = 0; i < prices.length; i++) {
             System.out.println(itemname[i] + "  " + prices[i] + "   " + quantity[i]);
         }
-
         input.close();
+        // end sabrina , jana
     }
 }
